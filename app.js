17b6ce6e52b451936f1d03cb19e96f8bd02587da
Joseph, John <jjoseph@cardinalsolutions.com>
2017-07-10 15:41:40 -0400

    Change port to 2000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 9000 : process.env.PORT;
+const port = isDeveloping ? 2000 : process.env.PORT;
 const app = express();
 
 if (isDeveloping) {
@@ -15,8 +15,8 @@ app.get('/', function response(req, res) {
 });
 
 app.listen(port, '0.0.0.0', function onStart(err) {
-    if (err) {
-            console.log(err);
-    }
-    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
